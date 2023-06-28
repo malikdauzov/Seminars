3729974f02e5e303abbd29aa13963a8d843e7494
Malik <astemirmail@gmail.com>
2023-06-28 15:31:17 +0600

    Task_002

---
+++ Task_002/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+int userInput = Convert.ToInt32(Console.ReadLine());
+if(userInput == 1)
+Console.WriteLine ("Monday");
+if(userInput == 2)
+Console.WriteLine ("Tuesday");
+if(userInput == 3)
+Console.WriteLine ("Wednesday");
+if(userInput == 4)
+Console.WriteLine ("Thursday");
+if(userInput == 5)
+Console.WriteLine ("Friday");
+if(userInput == 6)
+Console.WriteLine ("Saturday");
+if(userInput == 7)
+Console.WriteLine ("Sunday");
+*/
