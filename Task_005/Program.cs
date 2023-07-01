dc99e51de91c1f473ee0b6c9dfcd68a30387b062
Malik <astemirmail@gmail.com>
2023-07-01 15:29:36 +0600

    Task_005

---
+++ Task_005/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+// Console.WriteLine("Input number");
+// int Number1 = Convert.ToInt32(Console.ReadLine());
+// int Number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (Number1 > Number2)
+// {
+//     Console.WriteLine(Number1);
+// }
+// else if(Number2 > Number1)
+// {
+//      Console.WriteLine(Number2);
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+
+
+
