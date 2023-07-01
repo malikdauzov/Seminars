326e7768fc59330a8bab68c611f0f2de9537cc93
Malik <astemirmail@gmail.com>
2023-07-01 15:30:01 +0600

    Task_006

---
+++ Task_006/Program.cs
@@ -0,0 +1,17 @@
+﻿// Console.WriteLine("Input number");
+// int Number1 = Convert.ToInt32(Console.ReadLine());
+// int Number2 = Convert.ToInt32(Console.ReadLine());
+// int Number3 = Convert.ToInt32(Console.ReadLine());
+
+// if (Number1 > Number2 && Number1 > Number2 )
+// {
+//     Console.WriteLine(Number1);
+// }
+//  if(Number2 > Number1 && Number2 > Number3 )
+// {
+//      Console.WriteLine(Number2);
+// }
+//  else if (Number3 > Number2 && Number3 > Number1 )
+//  {
+//      Console.WriteLine(Number3);
+//  }
