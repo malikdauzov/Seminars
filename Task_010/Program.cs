2f9a4231d78c760d82597e787a548af2ca2b87d5
Malik <astemirmail@gmail.com>
2023-07-05 20:47:40 +0600

    Task_010

---
+++ Task_010/Program.cs
@@ -0,0 +1,7 @@
+﻿ int randomNumber = new Random(). Next(111,999);
+
+ 
+ int numberOne = randomNumber / 10;
+ int result = numberOne % 10 ;
+ Console.WriteLine(randomNumber);
+ Console.WriteLine(result);
